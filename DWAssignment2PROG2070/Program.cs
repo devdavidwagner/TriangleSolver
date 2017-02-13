f280901bc96adb069a3565018a358978d2e09a2e
David Wagner <david.karl.wagner@live.com>
2017-02-13 12:55:48 -0500

    Finished code. Now onto unit testing

--- DWAssignment2PROG2070/Program.cs
+++ DWAssignment2PROG2070/Program.cs
@@ -9,7 +9,10 @@ using System.Threading.Tasks;
  * ASSIGNMENT 2
  * Simple console program to create a triangle and detect its type
  * 
- * CREATED: 2/13/2017
+ * CREATED: 2/13/2017 12:00PM
+ * FINISHED: Program.cs and TriangleSolver.cs 
+ * 
+ * 
  * 
  * */
 
@@ -20,8 +23,78 @@ namespace DWAssignment2PROG2070
         static void Main(string[] args)
         {
 
-            
+            string choice = "";
+            int a, b, c = 0;
+            while (true)
+            {
+                Console.WriteLine("Welcome to the Triangle Solver!\n"
+                + "<--------------------------------------->\n" 
+                + "1. Enter triangle dimensions\n" 
+                + "2. Exit\n"
+                 + "<--------------------------------------->\n");
 
+            choice = Console.ReadLine();
+          
+                if (choice != "1" && choice != "2")
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("\nPlease choose 1 or 2 \n");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    switch (choice)
+                    {
+
+                        case "1":
+                            while (true)
+                            {
+                                Console.WriteLine("Please enter three integers:\n");
+
+                                try
+                                {
+                                    a = int.Parse(Console.ReadLine());
+                                    b = int.Parse(Console.ReadLine());
+                                    c = int.Parse(Console.ReadLine());
+
+                                    string message = TriangleSolver.Analyze(a, b, c);
+
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    Console.WriteLine("\n" + message + "\n");
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    break;
+
+                                }
+
+                                catch (Exception e)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Cyan;
+                                    Console.WriteLine("\n!--Please enter a numeric value--!\n");
+                                    Console.ForegroundColor = ConsoleColor.White;
+
+                                }
+                            }
+
+
+
+                            break;
+
+                        case "2":
+
+                            Environment.Exit(0);
+                            break;
+
+                        default:
+
+                            break;
+                    }
+
+
+
+                }
+            }
+              
+            
 
 
 
--- DWAssignment2PROG2070/TriangleSolver.cs
+++ DWAssignment2PROG2070/TriangleSolver.cs
@@ -8,5 +8,50 @@ namespace DWAssignment2PROG2070
 {
     class TriangleSolver
     {
+      
+        /// <summary>
+        /// Accepts three integers for triangle sides and returns a string 
+        /// </summary>
+        /// <param name="a">side 1</param>
+        /// <param name="b"> side 2</param>
+        /// <param name="c">side 3</param>
+        /// <returns>whether integers can return a triangle and what type of triangle it is</returns>
+        public static string Analyze(int a, int b, int c)
+        {
+            string message = "";
+
+
+            //triangle inequality theorem
+            if (a + b > c && a + c > b && b +c > a) 
+            {
+                if(a == b && a == c && b == c)
+                {
+                    message = "Your triangle is an equilateral triangle";
+                }
+                else if (a == b || b == c || a == c)
+                {
+                    message = "Your triangle is an isosceles triangle";
+                }
+                else if (a != b && b != c && c != a)
+                {
+                    message = "Your triangle is a scalene triangle";
+                }
+            }
+            else
+            {
+                message = "These integers do not form a triangle!";
+            }
+
+
+
+
+
+            return message;
+
+
+
+        }
+
+
     }
 }
