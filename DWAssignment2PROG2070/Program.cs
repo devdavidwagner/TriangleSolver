153c54ef9e9282755cf104fd4d8fbe252e99123a
David Wagner <david.karl.wagner@live.com>
2017-02-13 12:16:32 -0500

    Created program. Initial commmit

---
+++ DWAssignment2PROG2070/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * DAVID WAGNER
+ * ASSIGNMENT 2
+ * Simple console program to create a triangle and detect its type
+ * 
+ * CREATED: 2/13/2017
+ * 
+ * */
+
+namespace DWAssignment2PROG2070
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+
+
+
+
+
+        }
+    }
+}
---
+++ DWAssignment2PROG2070/TriangleSolver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DWAssignment2PROG2070
+{
+    class TriangleSolver
+    {
+    }
+}
